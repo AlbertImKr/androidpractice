37e1748df698587e0ef2ed76d1c422d91013f8ce
ALbertIM0427 <guang7226961@naver.com>
2025-03-30 23:02:32 +0900

    :sparkles: StopWatch

--- practice/app/src/main/java/com/albert/practice/MainActivity.kt
+++ practice/app/src/main/java/com/albert/practice/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.albert.practice
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.SystemClock
+import android.view.KeyEvent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.albert.practice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var initTime = 0L
+    private var pauseTime = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.startButton.setOnClickListener {
+            binding.chronometer.base = SystemClock.elapsedRealtime() + pauseTime
+            binding.chronometer.start()
+
+            binding.stopButton.isEnabled = true
+            binding.resetButton.isEnabled = true
+            binding.startButton.isEnabled = false
         }
+
+        binding.stopButton.setOnClickListener {
+            pauseTime = binding.chronometer.base - SystemClock.elapsedRealtime()
+            binding.chronometer.stop()
+
+            binding.stopButton.isEnabled = false
+            binding.resetButton.isEnabled = true
+            binding.startButton.isEnabled = true
+        }
+
+        binding.resetButton.setOnClickListener {
+            binding.chronometer.base = SystemClock.elapsedRealtime()
+            pauseTime = 0L
+            binding.chronometer.stop()
+
+            binding.stopButton.isEnabled = false
+            binding.resetButton.isEnabled = false
+            binding.startButton.isEnabled = true
+        }
+
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && SystemClock.elapsedRealtime() - initTime < 3000) {
+            Toast.makeText(this, "종료하려면 한 번 더 누르세요", Toast.LENGTH_SHORT).show()
+            initTime = System.currentTimeMillis()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
